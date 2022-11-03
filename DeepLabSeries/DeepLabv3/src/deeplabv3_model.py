9fd69eef8b5549416ca25f0a146d338eb8a4c044
weijingchao <13066010060@163.com>
2022-11-03 22:38:57 +0800

    2022.11.3 DeepLabv3_2

--- DeepLabSeries/DeepLabv3/src/deeplabv3_model.py
+++ DeepLabSeries/DeepLabv3/src/deeplabv3_model.py
@@ -7,6 +7,41 @@ from .resnet_backbone import resnet50, resnet101
 from .mobilenet_backbone import mobilenet_v3_large
 
 
+class IntermediateLayerGetter(nn.ModuleDict):
+
+    _version = 2
+    __annotations__ = {
+        "return_layers": Dict[str, str],
+    }
+
+    def __init__(self, model: nn.Module, return_layers: Dict[str, str]) -> None:
+        if not set(return_layers).issubset([name for name, _ in model.named_children()]):
+            raise ValueError("return_layers are not present in model")
+        orig_return_layers = return_layers
+        return_layers = {str(k): str(v) for k, v in return_layers.items()}
+
+        layers = OrderedDict()
+        for name, module in model.named_children():
+            layers[name] = module
+            if name in return_layers:
+                del return_layers[name]
+            if not return_layers:
+                break
+
+        super(IntermediateLayerGetter, self).__init__(layers)
+        self.return_layers = orig_return_layers
+
+
+    def forward(self, x: Tensor) -> Dict[str, Tensor]:
+        out = OrderedDict()
+        for name, module in self.items():
+            x = module(x)
+            if name in self.return_layers:
+                out_name = self.return_layers[name]
+                out[out_name] = x
+        return out
+
+
 class DeepLabv3(nn.Module):
 
     __constants__ = ['aux_classifier']
@@ -33,16 +68,169 @@ class DeepLabv3(nn.Module):
         return result
 
 
-def deeplabv3_resnet50():
-    pass
+class FCNHead(nn.Sequential):
+    def __init__(self, in_channels, channels):
+        inter_channels = in_channels // 4
+        super(FCNHead, self).__init__(
+            nn.Conv2d(in_channels, inter_channels, 3, padding=1, bias=False),
+            nn.BatchNorm2d(inter_channels),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Conv2d(inter_channels, channels, 1)
+        )
 
 
-def deeplabv3_resnet101():
-    pass
+class ASPPConv(nn.Sequential):
+    def __int__(self, in_channels: int, out_channels: int, dilation: int) -> None:
+        super(ASPPConv, self).__init__(
+            nn.Conv2d(in_channels, out_channels, 3, padding=dilation, dilation=dilation, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU()
+        )
 
 
-def deeplabv3_mobilenetv3_large():
-    pass
+class ASPPPooling(nn.Sequential):
+    def __int__(self, in_channels: int, out_channels: int) -> None:
+        super(ASPPPooling, self).__init__(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(in_channels, out_channels, 1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU()
+        )
 
 
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        size = x.shape[-2:]
+        for mod in self:
+            x = mod(x)
+        return F.interpolate(x, size=size, mode='bilinear', align_corners=False)
 
+
+class ASPP(nn.Module):
+    def __init__(self, in_channels: int, atrous_rates: List[int], out_channels: int = 256) -> None:
+        super(ASPP, self).__init__()
+        modules = [
+            nn.Sequential(nn.Conv2d(in_channels, out_channels, 1, bias=False),
+                          nn.BatchNorm2d(out_channels),
+                          nn.ReLU())
+        ]
+
+        rates = tuple(atrous_rates)
+        for rate in rates:
+            modules.append(ASPPConv(in_channels, out_channels, rate))
+
+        modules.append(ASPPPooling(in_channels, out_channels))
+
+        self.convs = nn.ModuleList(modules)
+
+        self.project = nn.Sequential(
+            nn.Conv2d(len(self.convs) * out_channels, out_channels, 1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+            nn.Dropout(0.5)
+        )
+
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        _res = []
+        for conv in self.convs:
+            _res.append(conv(x))
+        res = torch.cat(_res, dim=1)
+        return self.project(res)
+
+
+class DeepLabHead(nn.Sequential):
+    def __init__(self, inchannels: int, num_classes: int) -> None:
+        super(DeepLabHead, self).__init__(
+            ASPP(inchannels, [12, 24, 36]),
+            nn.Conv2d(256, 256, 3, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.Conv2d(256, num_classes, 1)
+        )
+
+
+def deeplabv3_resnet50(aux, num_classes=21, pretrain_backbone=False):
+    backbone = resnet50(replace_stride_with_dilation=[False, True, True])
+
+    if pretrain_backbone:
+        backbone.load_state_dict(torch.load("resnet50.pth", map_location='cpu'))
+
+    out_inplanes = 2048
+    aux_inplanes = 1024
+
+    return_layers = {'layer4': 'out'}
+    if aux:
+        return_layers['layer3'] = 'aux'
+    backbone = IntermediateLayerGetter(backbone, return_layers=return_layers)
+
+    aux_classifier = None
+    if aux:
+        aux_classifier = FCNHead(aux_inplanes, num_classes)
+
+    classifier = DeepLabHead(out_inplanes, num_classes)
+
+    model = DeepLabv3(backbone, classifier, aux_classifier)
+
+    return model
+
+
+def deeplabv3_resnet101(aux, num_classes=21, pretrain_backbone=False):
+    backbone = resnet101(replace_stride_with_dilation=[False, True, True])
+
+    if pretrain_backbone:
+        backbone.load_state_dict(torch.load("resnet101.pth", map_location='cpu'))
+
+    out_inplanes = 2048
+    aux_inplanes = 1024
+
+    return_layers = {'layer4': 'out'}
+    if aux:
+        return_layers['layer3'] = 'aux'
+    backbone = IntermediateLayerGetter(backbone, return_layers=return_layers)
+
+    aux_classifier = None
+    if aux:
+        aux_classifier = FCNHead(aux_inplanes, num_classes)
+
+    classifier = DeepLabHead(out_inplanes, num_classes)
+
+    model = DeepLabv3(backbone, classifier, aux_classifier)
+
+    return model
+
+
+def deeplabv3_mobilenetv3_large(aux, num_classes=21, pretrain_backbone=False):
+    # 'mobilenetv3_large_imagenet': 'https://download.pytorch.org/models/mobilenet_v3_large-8738ca79.pth'
+    # 'depv3_mobilenetv3_large_coco': "https://download.pytorch.org/models/deeplabv3_mobilenet_v3_large-fc3c493d.pth"
+    backbone = mobilenet_v3_large(dilated=True)
+
+    if pretrain_backbone:
+        # 载入mobilenetv3 large backbone预训练权重
+        backbone.load_state_dict(torch.load("mobilenet_v3_large.pth", map_location='cpu'))
+
+    backbone = backbone.features
+
+    # Gather the indices of blocks which are strided. These are the locations of C1, ..., Cn-1 blocks.
+    # The first and last blocks are always included because they are the C0 (conv1) and Cn.
+    stage_indices = [0] + [i for i, b in enumerate(backbone) if getattr(b, "is_strided", False)] + [len(backbone) - 1]
+    out_pos = stage_indices[-1]  # use C5 which has output_stride = 16
+    out_inplanes = backbone[out_pos].out_channels
+    aux_pos = stage_indices[-4]  # use C2 here which has output_stride = 8
+    aux_inplanes = backbone[aux_pos].out_channels
+    return_layers = {str(out_pos): "out"}
+    if aux:
+        return_layers[str(aux_pos)] = "aux"
+
+    backbone = IntermediateLayerGetter(backbone, return_layers=return_layers)
+
+    aux_classifier = None
+    # why using aux: https://github.com/pytorch/vision/issues/4292
+    if aux:
+        aux_classifier = FCNHead(aux_inplanes, num_classes)
+
+    classifier = DeepLabHead(out_inplanes, num_classes)
+
+    model = DeepLabv3(backbone, classifier, aux_classifier)
+
+    return model
